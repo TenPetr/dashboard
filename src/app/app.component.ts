ffd368b212af18dd87e17304bc369c6383fa95b3
Petr Jelínek <peetr.jelinek@gmail.com>
2019-11-09 10:58:36 +0100

    Added login and register page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,27 @@
-import { Component } from '@angular/core';
-
-import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Component } from "@angular/core";
+import { Router } from "@angular/router";
+import { Platform } from "@ionic/angular";
+import { SplashScreen } from "@ionic-native/splash-screen/ngx";
+import { StatusBar } from "@ionic-native/status-bar/ngx";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss']
+  selector: "app-root",
+  templateUrl: "app.component.html",
+  styleUrls: ["app.component.scss"]
 })
 export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private router: Router
   ) {
     this.initializeApp();
   }
 
   initializeApp() {
     this.platform.ready().then(() => {
+      this.router.navigateByUrl("login");
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
